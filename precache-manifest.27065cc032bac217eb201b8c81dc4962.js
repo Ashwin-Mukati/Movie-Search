198cd837b0d6538f1a45ecdafb99ed4e8ee76083
Ashwin-Mukati <ashwinmukati40@gmail.com>
2019-12-01 19:46:06 +0530

    Updates

---
+++ precache-manifest.27065cc032bac217eb201b8c81dc4962.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2eba0372968a19d86a96",
+    "url": "/Movie-Search/static/css/main.5eca5de5.chunk.css"
+  },
+  {
+    "revision": "2eba0372968a19d86a96",
+    "url": "/Movie-Search/static/js/main.9c87f4be.chunk.js"
+  },
+  {
+    "revision": "5d6291419a1179634611",
+    "url": "/Movie-Search/static/js/runtime~main.2f6b2dbd.js"
+  },
+  {
+    "revision": "defe3dee541e053917f0",
+    "url": "/Movie-Search/static/js/2.de17a4a9.chunk.js"
+  },
+  {
+    "revision": "ef5a165b7b9856300874bb8ce70c48b9",
+    "url": "/Movie-Search/index.html"
+  }
+];
